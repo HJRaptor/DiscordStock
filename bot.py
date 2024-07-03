cdf22d1c454cc27505d41986420307f6dabb5c89
harshit <harshit.jagarlamudi@outlook.com>
2024-07-03 22:36:53 +0100

    implement searching for a stock's price

--- bot.py
+++ bot.py
@@ -1,8 +1,6 @@
 import discord
-from discord import Member
-
-
-
+from discord import member
+import yfinance
 
 #intents
 intents = discord.Intents.all()
@@ -11,6 +9,16 @@ intents.members = True
 bot = discord.Bot(intents=intents)
 
 
+@bot.event
+async def on_ready():
+    print(f"We have logged in as {bot.user}")
 
+@bot.slash_command(guild_ids=[903618670700417065])
+async def info(ctx, symbol: str):
+    selectedStock = yfinance.Ticker(symbol)
+    regular_market_open = selectedStock.info['regularMarketOpen']
+    await ctx.respond(regular_market_open)
+    
 
+bot.run()
 
