4aa1cc894a74b96a3cd7f34a6d2898f33042a64d
harshit <harshit.jagarlamudi@outlook.com>
2024-07-03 21:43:35 +0100

    testing

--- bot.py
+++ bot.py
@@ -1,9 +1,7 @@
 import discord
 from discord import Member
 
-msft = yf.Ticker("MSFT")
 
-print(msft.info)
 
 
 #intents
@@ -12,9 +10,7 @@ intents.presences = True
 intents.members = True
 bot = discord.Bot(intents=intents)
 
-#@bot.slash_command(guild_ids=[903618670700417065])
-#async def start(ctx):
-    
+
 
 
 
