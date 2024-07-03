8d4362467c9c369ef7f659a7ab8d54fe050d9bfe
harshit <harshit.jagarlamudi@outlook.com>
2024-07-03 22:53:54 +0100

    created embed

--- bot.py
+++ bot.py
@@ -14,12 +14,26 @@ bot = discord.Bot(intents=intents)
 @bot.slash_command(guild_ids=[903618670700417065])
 async def info(ctx, symbol: str):
     selectedStock = yfinance.Ticker(symbol)
+
+    name = selectedStock.info['shortName']
     regular_market_open = selectedStock.info['regularMarketOpen']
-    await ctx.respond(regular_market_open)
+    daylow = selectedStock.info['dayLow']
+    dayhigh = selectedStock.info['dayHigh']
+
+    stockembed = discord.Embed(title=name,color=discord.Colour.blurple())
+    stockembed.add_field(name="Market Open",value=regular_market_open)
+    stockembed.add_field(name="Day Low", value=daylow)
+    stockembed.add_field(name="Day Low", value=dayhigh)
+    stockembed.set_footer(text="HJRaptor - 2024")
+    stockembed.ad
+
+    
+    await ctx.respond(embed=stockembed)
     
 
 @bot.event
 async def on_ready():
     print(f"We have logged in as {bot.user}")
+
 bot.run("MTI1ODE2MjcxNzY3MjYwNzg5Ng.GERP2P.Ul9xs7hwQ62nsokNt0DMBWdjEWRHVyc0GIL6yo")
 
