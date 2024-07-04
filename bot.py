741b2769b63f2770f81bef8ca11c99370449b80d
harshit <harshit.jagarlamudi@outlook.com>
2024-07-04 17:36:19 +0100

    add dividend yield

--- bot.py
+++ bot.py
@@ -23,17 +23,29 @@ async def info(ctx, symbol: str):
     regular_market_open = selectedStock.info['regularMarketOpen']
     daylow = selectedStock.info['dayLow']
     dayhigh = selectedStock.info['dayHigh']
+    divYield = float(selectedStock.info['dividendYield'])*100
+    divYield = round(divYield, 2)
 
     stockembed = discord.Embed(title=name,color=discord.Colour.blurple())
     stockembed.add_field(name="Market Open",value=regular_market_open)
     stockembed.add_field(name="Day Low", value=daylow)
     stockembed.add_field(name="Day Low", value=dayhigh)
+    stockembed.add_field(name="Annual Dividend Yield", value=divYield+"%")
     stockembed.set_footer(text="HJRaptor - 2024")
     
 
     
     await ctx.respond(embed=stockembed)
     
+
+@bot.slash_command(guild_ids=[903618670700417065])
+async def portfolio(ctx, symbol: str):
+    
+    
+
+    
+    await ctx.respond()
+
 
 @bot.event
 async def on_ready():
